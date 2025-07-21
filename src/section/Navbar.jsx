db1fdcecee340ea7b7bed3f81237bd5c6ad7bd7c
Jeniya14 <jeniyaselvam1432@gmail.com>
2025-07-21 22:07:23 +0530

    Navbar update

--- src/section/Navbar.jsx
+++ src/section/Navbar.jsx
@@ -2,6 +2,10 @@ const Navbar = () => {
   return (
      <nav className="p-4 shadow-md">
       <h1 className="text-xl font-bold">Jeni's Portfolio</h1>
+      <a>Home</a>
+      <a>about</a>
+      <a>work</a>
+      <a>contact</a>
     </nav>
   )
 }
