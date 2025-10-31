c91464ad413e617d6b0550b6f2f97b831fccecf0
Jeniya14 <jeniyaselvam1432@gmail.com>
2025-10-31 23:25:40 +0530

    Hero page update

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import './App.css'
+import GlowCurser from './components/GlowCurser'
 import About from './section/About'
 import Hero from './section/Hero'
 import Navbar from './section/Navbar'
+
 
 function App() {
  
 
   return (
-    <div className="bg-black text-gray-800">
+    <div className="bg-midnight text-white">
+      <GlowCurser />
       <Navbar/>
       <Hero />
       <About/>       
--- src/section/Hero.jsx
+++ src/section/Hero.jsx
@@ -1,6 +1,6 @@
 const Hero = () => {
   return (
-      <section className="min-h-screen flex flex-col items-center justify-center bg-black text-center px-4">
+      <section className="min-h-screen flex flex-col items-center justify-center text-center px-4">
       <h2 className="text-4xl font-bold mb-4">Hi, I'm Jeni</h2>
       <p className="text-lg max-w-md">
         A frontend developer crafting beautiful, responsive web experiences with React and Tailwind CSS.
