8f8d154e78a3c0142c9bdde8cfdb1d793b9dcca9
Jeniya14 <jeniyaselvam1432@gmail.com>
2025-11-18 12:38:01 +0530

    Project skill section update

--- src/section/Project.jsx
+++ src/section/Project.jsx
@@ -11,75 +11,101 @@ const Project = () => {
       {/* 1 */}
       <li className="min-h-[16rem] list-none md:[grid-area:1/1/2/7] xl:[grid-area:1/1/2/5]">
         <div className="project-card">
-           <button className="w-9 h-9 flex items-center justify-center rounded-full bg-gray-800 hover:bg-gray-700 transition">
-              →
-            </button>
+          <div className="w-full bg-gray-900 p-5 text-center">
+            <img src="/assets/food.png" alt="Carticon" className="w-20  mx-auto"/>
+          </div>
+           
             <div>
-              <h3 className="text-xl font-semibold">Gameverse</h3>
+              <h3 className="text-xl font-semibold">Foodie</h3>
               <p className="text-gray-400 mt-2">
                 Where user can manage their games and see the reviews.
               </p>
             </div>
+            <button className=" absolute bottom-4 right-4 w-9 h-9 flex items-center justify-center rounded-xl bg-gray-800 hover:bg-gray-700 transition">
+              <img src="/assets/github.svg" alt="github" className="w-6" />
+            </button>
         </div>
       </li>
 
       {/* 2 */}
       <li className="min-h-[16rem] list-none md:[grid-area:1/7/2/13] xl:[grid-area:2/1/3/5]">
         <div className="project-card">
-           <button className="w-9 h-9 flex items-center justify-center rounded-full bg-gray-800 hover:bg-gray-700 transition">
-              →
-            </button>
+          <div className="w-full bg-gray-900 p-5 text-center">
+            <img src="/assets/crm.png" alt="Carticon" className="w-20  mx-auto"/>
+          </div>
+           
             <div>
-              <h3 className="text-xl font-semibold">Gameverse</h3>
+              <h3 className="text-xl font-semibold">Soulmate</h3>
               <p className="text-gray-400 mt-2">
                 Where user can manage their games and see the reviews.
               </p>
+              <ul className="project-skill"><li>React native</li><li>React native</li><li>React native</li><li></li></ul>
             </div>
+            <button className="absolute bottom-4 right-4 w-9 h-9 flex items-center justify-center rounded-xl bg-gray-800 hover:bg-gray-700 transition">
+              <img src="/assets/github.svg" alt="github" className="w-6" />
+            </button>
         </div>
       </li>
 
       {/* 3 (middle, larger one) */}
       <li className="min-h-[18rem] list-none md:[grid-area:2/1/3/7] xl:[grid-area:1/5/3/8]">
         <div className="project-card">
-           <button className="w-9 h-9 flex items-center justify-center rounded-full bg-gray-800 hover:bg-gray-700 transition">
-              →
-            </button>
+          <div className="w-full bg-gray-900 p-5 text-center">
+            <img src="/assets/carticon.png" alt="Carticon" className="w-20  mx-auto"/>
+          </div>
+           
             <div>
-              <h3 className="text-xl font-semibold">Gameverse</h3>
+              <h3 className="text-xl font-semibold">Shopcart</h3>
               <p className="text-gray-400 mt-2">
                 Where user can manage their games and see the reviews.
               </p>
             </div>
+            <button className="absolute bottom-4 right-4 w-9 h-9 flex items-center justify-center rounded-xl bg-gray-800 hover:bg-gray-700 transition">
+              <img src="/assets/github.svg" alt="github" className="w-6" />
+            </button>
         </div>
       </li>
 
       {/* 4 */}
       <li className="min-h-[16rem] list-none md:[grid-area:2/7/3/13] xl:[grid-area:1/8/2/13]">
         <div className="project-card">
-           <button className="w-9 h-9 flex items-center justify-center rounded-full bg-gray-800 hover:bg-gray-700 transition">
-              →
-            </button>
+          <div className="w-full bg-gray-900 p-5 text-center">
+            <img src="/assets/game1.png" alt="Carticon" className="w-20  mx-auto"/>
+          </div>
+           
             <div>
-              <h3 className="text-xl font-semibold">Gameverse</h3>
+              <h3 className="text-xl font-semibold">Gamng</h3>
               <p className="text-gray-400 mt-2">
                 Where user can manage their games and see the reviews.
               </p>
             </div>
+            <button className="absolute bottom-4 right-4 w-9 h-9 flex items-center justify-center rounded-xl bg-gray-800 hover:bg-gray-700 transition">
+              <img src="/assets/github.svg" alt="github" className="w-6" />
+            </button>
         </div>
       </li>
 
       {/* 5 */}
       <li className="min-h-[16rem] list-none md:[grid-area:3/1/4/13] xl:[grid-area:2/8/3/13]">
         <div className="project-card">
-           <button className="w-9 h-9 flex items-center justify-center rounded-full bg-gray-800 hover:bg-gray-700 transition">
-              →
-            </button>
+          <div className="w-full bg-gray-900 p-5 text-center">
+            <img src="/assets/sellyard1.png" alt="Carticon" className="w-20  mx-auto"/>
+          </div>
+           
             <div>
-              <h3 className="text-xl font-semibold">Gameverse</h3>
+              <h3 className="text-xl font-semibold">Sellyard</h3>
               <p className="text-gray-400 mt-2">
                 Where user can manage their games and see the reviews.
               </p>
+              {/* <ul>
+                <li>react</li>
+                <li>native</li>
+                <li>matrial ui</li>
+              </ul> */}
             </div>
+            <button className="absolute bottom-4 right-4 w-9 h-9 flex items-center justify-center rounded-xl bg-gray-800 hover:bg-gray-700 transition">
+              <img src="/assets/github.svg" alt="github" className="w-6" />
+            </button>
         </div>
       </li>
     </ul>
