0bffc9d644d7fc5db544e5a32acc26649b864b9b
Jeniya14 <jeniyaselvam1432@gmail.com>
2025-11-18 12:37:52 +0530

    icon update

--- src/section/Project.jsx
+++ src/section/Project.jsx
@@ -1,7 +1,7 @@
 const Project = () => {
   return (
     <div id="projects" className="min-h-screen text-white">
-  <h1 className="text-3xl sm:text-4xl font-bold px-4 sm:px-10 md:px-20">
+  <h1 className="text-3xl sm:text-4xl font-bold px-4 sm:px-10 md:px-20 text-center">
     My Projects
   </h1>
 
@@ -9,7 +9,7 @@ const Project = () => {
     <ul className="grid grid-cols-1 md:grid-cols-12 gap-4 xl:grid-rows-2">
       
       {/* 1 */}
-      <li className="min-h-[14rem] list-none md:[grid-area:1/1/2/7] xl:[grid-area:1/1/2/5]">
+      <li className="min-h-[16rem] list-none md:[grid-area:1/1/2/7] xl:[grid-area:1/1/2/5]">
         <div className="project-card">
            <button className="w-9 h-9 flex items-center justify-center rounded-full bg-gray-800 hover:bg-gray-700 transition">
               →
@@ -24,7 +24,7 @@ const Project = () => {
       </li>
 
       {/* 2 */}
-      <li className="min-h-[14rem] list-none md:[grid-area:1/7/2/13] xl:[grid-area:2/1/3/5]">
+      <li className="min-h-[16rem] list-none md:[grid-area:1/7/2/13] xl:[grid-area:2/1/3/5]">
         <div className="project-card">
            <button className="w-9 h-9 flex items-center justify-center rounded-full bg-gray-800 hover:bg-gray-700 transition">
               →
@@ -54,7 +54,7 @@ const Project = () => {
       </li>
 
       {/* 4 */}
-      <li className="min-h-[14rem] list-none md:[grid-area:2/7/3/13] xl:[grid-area:1/8/2/13]">
+      <li className="min-h-[16rem] list-none md:[grid-area:2/7/3/13] xl:[grid-area:1/8/2/13]">
         <div className="project-card">
            <button className="w-9 h-9 flex items-center justify-center rounded-full bg-gray-800 hover:bg-gray-700 transition">
               →
@@ -69,7 +69,7 @@ const Project = () => {
       </li>
 
       {/* 5 */}
-      <li className="min-h-[14rem] list-none md:[grid-area:3/1/4/13] xl:[grid-area:2/8/3/13]">
+      <li className="min-h-[16rem] list-none md:[grid-area:3/1/4/13] xl:[grid-area:2/8/3/13]">
         <div className="project-card">
            <button className="w-9 h-9 flex items-center justify-center rounded-full bg-gray-800 hover:bg-gray-700 transition">
               →
